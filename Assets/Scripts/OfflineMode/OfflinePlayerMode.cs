8fe3cd7293356e667a05184f3b46a4d0ef81d4c1
FaresBakr51 <faresbakr5151@gmail.com>
2022-02-23 00:54:21 +0200

    add

--- Assets/Scripts/OfflineMode/OfflinePlayerMode.cs
+++ Assets/Scripts/OfflineMode/OfflinePlayerMode.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 public class OfflinePlayerMode : MonoBehaviour
 {
   [SerializeField] private Transform[] _spawnPoints;
@@ -12,22 +15,36 @@ public class OfflinePlayerMode : MonoBehaviour
  public GameObject[] _scoreBoard;
   public List<GameObject> _controllcanavas = new List<GameObject>();
  public List<GameObject> _CurrentPlayers = new List<GameObject>();
+ public GameObject player1,player2;
+ public GameObject _player1Firstchar;
+ public GameObject _player2Firstchar;
+ public GameObject _SelectorPane;
+ public GameObject _startButt;
+ [SerializeField] private GameObject[] _player2Selector;
+ [SerializeField] private GameObject[] _SelectionButtonsPlayer1;
+ [SerializeField] private GameObject[] _SelectionButtonsPlayer2;
+
     void Awake()
     {
-    //  PhotonNetwork.OfflineMode = true;
+      PhotonNetwork.OfflineMode = true;
       /* if(PhotonNetwork.OfflineMode == true &&PhotonNetwork.InRoom == true){
 
           PhotonNetwork.Instantiate("PhotonNetworkAvatar",transform.position,transform.rotation, 0);
       } */
     
          if(PhotonNetwork.OfflineMode == true && PhotonNetwork.InRoom == false){ 
-
-           GameObject player1 =   Instantiate(_playersObj[0],_spawnPoints[0].transform.position,_spawnPoints[0].transform.rotation);
+           _SelectorPane.SetActive(true);
+               foreach(GameObject obj in _player2Selector){
+        obj.SetActive(false);
+      }
+           EventSystem.current.SetSelectedGameObject(_player1Firstchar);
+       
+         /*   GameObject player1 =   Instantiate(_playersObj[0],_spawnPoints[0].transform.position,_spawnPoints[0].transform.rotation);
             GameObject player2 =  Instantiate(_playersObj[1],_spawnPoints[1].transform.position,_spawnPoints[1].transform.rotation);
             _CurrentPlayers.Add(player1);
-            _CurrentPlayers.Add(player2);
+            _CurrentPlayers.Add(player2); */
          
-             SwitchControll();
+             //SwitchControll();
         }else{
 
           foreach(GameObject obj in offlineModeobj ){
@@ -40,13 +57,60 @@ public class OfflinePlayerMode : MonoBehaviour
 
     }
 
-    
-    
-    public void SwitchControll(){
+    public void SelectCharacter1(string pl){
 
-    /*     _CurrentPlayers[0].GetComponent<PlayerController>().enabled = true;
-          _CurrentPlayers[1].GetComponent<PlayerController>().enabled = true; */
-         // _CurrentPlayers[1].GetComponent<PhotonView>().ControllerActorNr = false;
+      switch(pl){
+
+        case "paul":
+        player1 = _playersObj[0];
+        break;
+        case "izzy":
+           player1 = _playersObj[2];
+        break;
+        case "mrbill":
+           player1 = _playersObj[1];
+        break;
+      }
+      foreach(GameObject obj in _player2Selector){
+        obj.SetActive(true);
+      }
+       foreach(GameObject obj in _SelectionButtonsPlayer1){
+        obj.GetComponent<Button>().interactable = true;
+      }
+        EventSystem.current.SetSelectedGameObject(_player2Firstchar);
+    }
+    public void SelectCharacter2(string pl){
+
+      switch(pl){
+
+        case "paul":
+        player2 = _playersObj[0];
+        break;
+        case "izzy":
+           player2 = _playersObj[2];
+        break;
+        case "mrbill":
+           player2 = _playersObj[1];
+        break;
+      }
+       foreach(GameObject obj in _SelectionButtonsPlayer2){
+        obj.GetComponent<Button>().interactable = true;
+      }
+        EventSystem.current.SetSelectedGameObject(_startButt);
+    }
+    public void StartGame(){
+      
+      _CurrentPlayers.Add(player1);
+      _CurrentPlayers.Add(player2);
+      _SelectorPane.SetActive(false);
+      SwitchControll();
+       Instantiate(player1,_spawnPoints[0].transform.position,_spawnPoints[0].transform.rotation);
+       Instantiate(player2,_spawnPoints[1].transform.position,_spawnPoints[1].transform.rotation);
+    }
+    public void Back(){
+      SceneManager.LoadScene(1);
+    }
+    public void SwitchControll(){
         _CurrentPlayers[0].GetComponent<PlayerControllOFFlineMode>()._mycontroll = 0;
           _CurrentPlayers[1].GetComponent<PlayerControllOFFlineMode>()._mycontroll = 1; 
         _CurrentPlayers[0].GetComponent<PlayerControllOFFlineMode>().myleader = _scoreBoard[0];
